4ff82962b81e1351a15092feffdb7f0b424e042a
fox.zhouzhou <kaifeizhou@outlook.com>
2019-07-17 23:51:01 +0800

    简易轮播图

--- js/index.js
+++ js/index.js
@@ -11,6 +11,27 @@
 
     /* 点击更换背景图片-end */
 
+    /* 头部我的网页动画 */
+    function autoGRWY() {
+      var index = 0
+      var width = $('.no-swiper-slide').eq(1).width();
+      setInterval(function () {
+        if (index == $('.no-swiper-slide').length - 1) {
+          index = 0;
+          $('.no-swiper-wrapper').css({
+            left: -index * width,
+          })
+        } else {
+          index++
+        }
+        $('.no-swiper-wrapper').animate({
+          left: -index * width,
+        }, 1000)
+      }, 2000)
+    }
+    autoGRWY()
+
+
 
 
     /* 导航栏按钮注册点击事件 */
@@ -22,24 +43,25 @@
     })
 
 
+
     /* css点击跳转响应的内容 */
     $('.content-css li').click(function () {
-     var css_index=  $(this).addClass('active').siblings().removeClass('active').end().index()
-console.log(css_index);
+      var css_index = $(this).addClass('active').siblings().removeClass('active').end().index()
+      console.log(css_index);
 
-     $.get('./md/02-css/css-day0'+css_index+'.md', function (text) {
-      /*  document.write(markdownit().render(text)); */
-     
-      $(' .content-show-md').html(
-        markdownit().render(text)
-      )
-    });
+      $.get('./md/02-css/css-day0' + css_index + '.md', function (text) {
+        /*  document.write(markdownit().render(text)); */
+
+        $(' .content-show-md').html(
+          markdownit().render(text)
+        )
+      });
 
     })
 
     $.get('./md/01-html/html.md', function (text) {
       /*  document.write(markdownit().render(text)); */
-     
+
       $(' .content-show-md').html(
         markdownit().render(text)
       )
